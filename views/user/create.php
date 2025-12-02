8ebc8ef75d5b94e226e752c80511ba7f15c8a12c
OMouloud <mouloudouldali@yahoo.fr>
2025-12-02 15:12:25 -0500

    resolution of the buy size of the inputs when displaying errors

--- views/user/create.php
+++ views/user/create.php
@@ -5,29 +5,33 @@
 
   <form action="{{ base }}/user/create" method="post" class="form__user">
     <h1>Créer un compte :</h1>
-    <label for="username">
-      Nom :
-      <input type="text" name="username" id="username" value="{{ user.username }}">
-      {% if errors.username is defined %}
-      <span class="error">{{ errors.username }}</span>
-      {% endif %}
-    </label>
-
-    <label for="email">
-      Email :
-      <input type="email" name="email" id="email" value="{{ user.email }}">
-      {% if errors.email is defined %}
-      <span class="error">{{ errors.email }}</span>
-      {% endif %}
-    </label>
-
-    <label for="password">
-      Mot de passe :
-      <input type="password" name="password" id="password">
-      {% if errors.password is defined %}
-      <span class="error">{{ errors.password }}</span>
-      {% endif %}
-    </label>
+    <div class="label-wrapper">
+      <label for="username">
+        Nom :
+        <input type="text" name="username" id="username" value="{{ user.username }}">
+        {% if errors.username is defined %}
+        <span class="error">{{ errors.username }}</span>
+        {% endif %}
+      </label>
+    </div>
+    <div class="label-wrapper">
+      <label for="email">
+        Email :
+        <input type="email" name="email" id="email" value="{{ user.email }}">
+        {% if errors.email is defined %}
+        <span class="error">{{ errors.email }}</span>
+        {% endif %}
+      </label>
+    </div>
+    <div class="label-wrapper">
+      <label for="password">
+        Mot de passe :
+        <input type="password" name="password" id="password">
+        {% if errors.password is defined %}
+        <span class="error">{{ errors.password }}</span>
+        {% endif %}
+      </label>
+    </div>
     <input type="submit" value="Créer le compte">
   </form>
 </div>
